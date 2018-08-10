2ed7ad4dd45604b0d73774f4b23416eab7e07e75
Victor Hugo Billi de Oliveira Dias <victor.dias@al.infnet.edu.br>
2018-08-10 12:07:57 -0300

    adicionando exercicio 5.3

--- exercicio5.py
+++ exercicio5.py
@@ -11,7 +11,26 @@ else:
 print(f'\n Primeira metade da Tupla: {times[0:2]}, Segunda metade da Tupla: {times[2:4]}\n')
 
 
+'''Questão 5.3'''
+print('''
+0 - Vasco
+1 - Flamengo
+2 - Fluminense
+3 - Botafogo
+''')
 
+excluir = int(input('Qual você deseja excluir da lista? \n'))
+
+tuplaAjuda = ()
+
+if(excluir < 0 or excluir > 3):
+    print('Elemento não existente')
+else:
+    for x in range(0,4):
+        if(x != excluir):
+            tuplaAjuda += (times[x],)
+
+print(tuplaAjuda)
 
 
 '''Questão 5.4'''
